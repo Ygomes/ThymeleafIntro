73e168e3ec9cd6caa78ecc5f4ca6058d7ee41eb7
yuri.oliveira <yuri.oliveira@me.com.br>
2022-01-27 18:17:31 -0300

    organização

--- demo/src/main/java/com/example/demo/Entity/Representante.java
+++ demo/src/main/java/com/example/demo/Entity/Representante.java
@@ -1,15 +1,11 @@
 package com.example.demo.Entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Representante {
 
     @Id
@@ -21,4 +17,65 @@ public class Representante {
 
     @ManyToOne(fetch = FetchType.LAZY)
     private Fornecedor fornecedor;
+
+    public Representante() {
+    }
+
+    public Representante(String nome, String cpf, String cargo, Fornecedor fornecedor) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.cargo = cargo;
+        this.fornecedor = fornecedor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public Fornecedor getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(Fornecedor fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+    @Override
+    public String toString() {
+        return "Representante{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", cargo='" + cargo + '\'' +
+                ", fornecedor=" + fornecedor +
+                '}';
+    }
 }
